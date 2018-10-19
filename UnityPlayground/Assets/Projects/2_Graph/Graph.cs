033a5b41cd44954e80de902e220d99c484d8ae96
Josef21296 <jcasanovasv@hotmail.com>
2018-10-19 16:41:04 +0200

    3D graph

--- UnityPlayground/Assets/Projects/2_Graph/Graph.cs
+++ UnityPlayground/Assets/Projects/2_Graph/Graph.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour 
 {
+    [SerializeField] private GraphFunctionName function;
     [SerializeField] private Transform pointPrefab;
     [SerializeField] [Range(10, 200)] private int resolution = 10;
-    [SerializeField] private Function function;
+
+    [SerializeField] private bool animated = false;
 
     private bool changed = false;
     private Transform[] points;
+
+    // ---
+
+    GraphFunction[] functions =
+    {
+        SineFunction, MultiSineFunction, Sine2DFunction, MultiSine2DFunction, RippleFunction
+    };
 
 
 	//========================================================
@@ -29,14 +38,17 @@ public class Graph : MonoBehaviour
             changed = false;
         }
         
-        if(function.animated)
+        if(animated)
         {
+            float t = Time.time;
+            GraphFunction f = functions[(int)function];
+
             for (int i = 0; i < points.Length; ++i)
             {
                 Transform point = points[i];
                 Vector3 position = point.position;
 
-                position.y = function.GetY(position.x);
+                position.y = f(position.x, position.z, t);
 
                 point.localPosition = position;
             }
@@ -70,26 +82,76 @@ public class Graph : MonoBehaviour
 
     void SetPoints()
     {
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
 
         float step = 2f / resolution;
         Vector3 scale = Vector3.one * step;
         Vector3 position = Vector3.zero;
 
-        for (int i = 0; i < resolution; i++)
+        GraphFunction f = functions[(int)function];
+
+        for(int i = 0, z = 0; z < resolution; ++z)
         {
-            Transform point = Instantiate(pointPrefab);
+            position.z = (z + 0.5f) * step - 1f;
 
-            position.x = (i + 0.5f) * step - 1f;
+            for (int x = 0; x < resolution; ++x, ++i)
+            {
+                Transform point = Instantiate(pointPrefab);
 
-            position.y = function.GetY(position.x);
+                position.x = (x + 0.5f) * step - 1f;
 
-            point.localPosition = position;
-            point.localScale = scale;
+                position.y = f(position.x, position.z, 0f);
 
-            point.SetParent(transform, false);
+                point.localPosition = position;
+                point.localScale = scale;
 
-            points[i] = point;
+                point.SetParent(transform, false);
+
+                points[i] = point;
+            }
         }
+    }
+
+    // ----
+
+    static float SineFunction(float x, float z, float t)
+    {
+        return Mathf.Sin((x + t) * Mathf.PI);
+    }
+
+    static float MultiSineFunction(float x, float z, float t)
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(2f * Mathf.PI * (x + 2f * t)) / 2f;
+        y *= 2f / 3f;
+
+        return y;
+    }
+
+    static float Sine2DFunction(float x, float z, float t)
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + t)) + Mathf.Sin(Mathf.PI * (z + t));
+        y *= 0.5f;
+
+        return y;
+    }
+
+    static float MultiSine2DFunction(float x, float z, float t)
+    {
+        float y = 4f * Mathf.Sin(Mathf.PI * (x + z + t * 0.5f));
+        y += Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(2f * Mathf.PI * (z + 2f * t)) * 0.5f;
+        y *= 1f / 5.5f;
+
+        return y;
+    }
+
+    static float RippleFunction(float x, float z, float t)
+    {
+        float d = Mathf.Sqrt(x * x + z * z);
+        float y = Mathf.Sin(Mathf.PI * (4f * d - t));
+        y /= 1f + 10f * d;
+
+        return y;
     }
 }
---
+++ UnityPlayground/Assets/Projects/2_Graph/GraphFunction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public delegate float GraphFunction (float x, float z, float t);
+
+public enum GraphFunctionName
+{
+    Sine,
+    MultiSine,
+    Sine2D,
+    MultiSine2D,
+    Ripple
+}
