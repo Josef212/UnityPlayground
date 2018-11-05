e0350c13b5ad20191e41cff01ac8e621d7fec7b6
Josef21296 <jcasanovasv@hotmail.com>
2018-11-05 16:57:22 +0100

    Hexagonal grid

---
+++ UnityPlayground/Assets/Projects/8_HexMap/Scripts/HexCell.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCell : MonoBehaviour 
+{
+    public HexCoordinates coordinates;
+
+    public Color color;
+
+	//========================================================
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+
+	//========================================================
+
+	
+}
