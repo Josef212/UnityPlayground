3022eaa055543fd86b6439f6c6f394c619329547
Josef21296 <jcasanovasv@hotmail.com>
2018-11-10 00:19:44 +0100

    Wall towers

--- UnityPlayground/Assets/Projects/8_HexMap/Scripts/HexMetrics.cs
+++ UnityPlayground/Assets/Projects/8_HexMap/Scripts/HexMetrics.cs
@@ -36,9 +36,12 @@ public static class HexMetrics
     public const int hashGridSize = 256;
     public const float hashGridScale = 0.25f;
 
-    public const float wallHeight = 3f;
+    public const float wallHeight = 4f;
     public const float wallThickness = 0.75f;
     public const float wallElevationOffset = verticalTerraceStepSize;
+    public const float wallYOffset = -1f;
+
+    public const float wallTowerThreshold = 0.5f;
 
     static HexHash[] hashGrid;
 
@@ -58,7 +61,7 @@ public static class HexMetrics
         near.x += (far.x - near.x) * 0.5f;
         near.z += (far.z - near.z) * 0.5f;
         float v = near.y < far.y ? wallElevationOffset : (1f - wallElevationOffset);
-        near.y += (far.y - near.y) * v;
+        near.y += (far.y - near.y) * v + wallYOffset;
 
         return near;
     }
