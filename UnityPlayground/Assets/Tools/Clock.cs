658f7661502f0e94667bce3eeb837a0332577973
Josef21296 <jcasanovasv@hotmail.com>
2019-01-19 21:07:54 +0100

    A couple of tools

---
+++ UnityPlayground/Assets/Tools/Clock.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GG
+{
+    public class Clock : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_visual = null;
+        [SerializeField] private Text m_clockText = null;
+
+        private float m_timer;
+        private Action m_callback = null;
+        private bool m_running;
+
+        public void StartClock(float time, Action callback)
+        {
+            m_timer = time;
+            m_callback = callback;
+            m_running = true;
+            m_visual.SetActive(true);
+
+            UpdateText();
+        }
+
+        public void StopClock()
+        {
+            m_running = false;
+            m_visual.SetActive(false);
+            m_callback = null;
+        }
+
+        private void Awake()
+        {
+            StopClock();
+        }
+
+        private void Update()
+        {
+            if(m_running)
+            {
+                m_timer -= Time.deltaTime;
+
+                if(m_timer <= 0)
+                {
+                    m_timer = 0;
+                    m_running = false;
+                    if (m_callback != null) m_callback();
+                    m_callback = null;
+                }
+
+                UpdateText();
+            }
+        }
+
+        private void UpdateText()
+        {
+            int minutes = (int)(m_timer / 60.0f);
+            int seconds = (int)(m_timer % 60.0f);
+            string zero = (seconds < 10) ? "0" : "";
+
+            m_clockText.text = minutes + ":" + zero + seconds;
+        }
+    }
+}
---
+++ UnityPlayground/Assets/Tools/FPSDisplay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace GG
+{
+    public class FPSDisplay : MonoBehaviour
+    {
+        private float deltaTime = 0.0f;
+        GUIStyle style = new GUIStyle();
+
+        private void Update()
+        {
+            deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        }
+
+        private void OnGUI()
+        {
+            int w = Screen.width, h = Screen.height;
+
+            Rect rect = new Rect(0, h - (h * 5 / 100), 2, h * 2 / 100);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * 3 / 100;
+            style.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            GUI.Label(rect, text, style);
+        }
+    }
+}
